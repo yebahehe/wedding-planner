82d4069fde1ea954e576781262925e72625ffd68
oinkoinkhehe <hsingni.c@gmail.com>
2024-11-12 16:34:10 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeddingPlanner from './WeddingPlanner';
+
+function App() {
+  return (
+    <div className="App">
+      <WeddingPlanner />
+    </div>
+  );
+}
+
+export default App;
